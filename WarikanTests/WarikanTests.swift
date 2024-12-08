a87b900b5a1bf3b590f152eae5918ebefe45a0b8
Mishima Haruto <harutomishima@MishimanoMacBook-Pro.local>
2024-12-08 12:13:57 +0900

    initial commit

---
+++ WarikanTests/WarikanTests.swift
@@ -0,0 +1,17 @@
+//
+//  WarikanTests.swift
+//  WarikanTests
+//
+//  Created by Mishima Haruto on 12/8/24.
+//
+
+import Testing
+@testable import Warikan
+
+struct WarikanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
